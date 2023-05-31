80f787ee5e3fca0e36ec25db654baaf640f8ceb5
samrood <samruofficial@gmail.com>
2023-05-31 19:58:56 +0530

    first commit

--- src/Componants/Mainbar/Mainbar.js
+++ src/Componants/Mainbar/Mainbar.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import FirstSection from '../Sections/FirstSection'
 
 const Mainbar = () => {
     return (
-        <div className='grid grid-cols-3 gap-4 py-5'>
-            <div className='col-span-2'>
-                <FirstSection />
-                <FirstSection />
-                {/* <FirstSection />
-                <FirstSection /> */}
+
+        <div class="grid grid-cols-3 gap-2 py-5">
+            <div class="col-span-2">
+                <div class="grid grid-cols-2 gap-4">
+                    <div class="bg-blue-500 text-white p-4">1</div>
+                    <div class="bg-green-500 text-white p-4">2</div>
+                    <div class="bg-green-500 text-white p-4">2</div>
+                    <div class="bg-green-500 text-white p-4">2</div>
+                </div>
             </div>
             <div className='col-span-1'>
-                <FirstSection />
+                <div class="bg-red-500 text-white p-4">3</div>
+                <div class="bg-red-500 text-white my-4 p-4">3</div>
             </div>
         </div>
     )
